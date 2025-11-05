3fe0cc2ccd24ad390433fae98132f0ec9253989c
yogesh kugan <yogesh.k6499@gmail.com>
2025-11-05 16:31:46 +0000

    added ping log

--- main.go
+++ main.go
@@ -56,6 +56,12 @@ func initDb() {
 	if err != nil {
 		log.Fatalf("Failed to open database: %v", err)
 	}
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := db.PingContext(ctx); err != nil {
+		log.Fatalf("Error pinging DB: %v", err)
+	}
 
 	// Retry with backoff (helps with slow DNS)
 	for i := 1; i <= 5; i++ {
