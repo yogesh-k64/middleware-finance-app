dcc7a09312b1d5d23aeab66317b16c05fabe62a0
yogesh kugan <yogesh.k6499@gmail.com>
2025-11-12 10:42:27 +0000

    added new user table

--- sql/migration-1.sql
+++ sql/migration-1.sql
@@ -16,3 +16,18 @@ CREATE INDEX idx_handouts_name ON handouts(name);
 CREATE INDEX idx_handouts_date ON handouts(date);
 CREATE INDEX idx_handouts_mobile ON handouts(mobile);
 CREATE INDEX idx_handouts_created_at ON handouts(created_at);
+
+CREATE OR REPLACE FUNCTION update_updated_at_column() 
+RETURNS TRIGGER AS $$
+BEGIN
+  NEW.updated_at = NOW();
+  RETURN NEW;
+END;
+$$ language 'plpgsql';
+
+CREATE TRIGGER update_handouts_updated_at 
+BEFORE UPDATE ON  handouts
+FOR EACH ROW
+EXECUTE FUNCTION update_updated_at_column();
+
+
--- sql/migration-2.sql
+++ sql/migration-2.sql
@@ -1,13 +1,20 @@
-CREATE OR REPLACE FUNCTION update_updated_at_column() 
-RETURNS TRIGGER AS $$
-BEGIN
-  NEW.updated_at = NOW();
-  RETURN NEW;
-END;
-$$ language 'plpgsql';
+DROP TABLE IF EXISTS users;
 
-CREATE TRIGGER update_handouts_updated_at 
-BEFORE UPDATE ON  handouts
-FOR EACH ROW
-EXECUTE FUNCTION update_updated_at_column();
+CREATE TABLE users (
+  id BIGINT PRIMARY KEY,
+  name TEXT NOT NULL,
+  phone_number BIGINT,
+  address TEXT,
+  info TEXT,
+  referred_by BIGINT,
+  created_at TIMESTAMPTZ DEFAULT NOW(),
+  updated_at TIMESTAMPTZ DEFAULT NOW(),
+  FOREIGN KEY (referred_by) REFERENCES users(id) ON DELETE SET NULL
+);
 
+CREATE INDEX idx_user_name ON users(name);
+CREATE INDEX idx_user_id ON users(id);
+
+CREATE TRIGGER update_users_updated_at
+BEFORE UPDATE ON users
+FOR EACH ROW EXECUTE FUNCTION update_updated_at_column();
